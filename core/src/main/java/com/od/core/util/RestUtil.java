e873bcf62f7b2a3ca5d7acbba1ab261184b63466
Super <minchao.dong@melot.cn>
2018-04-25 17:09:13 +0800

    feature: 判空方法

--- core/src/main/java/com/od/core/util/RestUtil.java
+++ core/src/main/java/com/od/core/util/RestUtil.java
@@ -1,5 +1,7 @@
 package com.od.core.util;
 
+import com.od.core.rest.NetParams;
+
 /**
  * Created by Super on 2017/9/12.
  */
@@ -9,5 +11,21 @@ public class RestUtil {
         if (object == null) {
             throw new IllegalArgumentException(error + "can not be null!");
         }
+    }
+
+    public static void checkNotNull(Object object) {
+        if (object == null) {
+            throw new IllegalStateException(object.getClass().getName() + "can not be null");
+        }
+    }
+
+    /**
+     * 填充字符串
+     * @param res
+     * @param objects
+     * @return
+     */
+    public static String getString(int res, Object... objects) {
+        return NetParams.getInstance().mApplicationContext.getString(res, objects);
     }
 }
